d3795b10e8c14d95bf300d0a811834007cb30364
Luís Gustavo <luisgsilva270@gmail.com>
2022-04-26 18:20:30 +0000

    refactor: models.py

--- beerlog/models.py
+++ beerlog/models.py
@@ -1,13 +1,34 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from pydantic import validator
+from statistics import validator
+from datetime import datetime
 
 
 class Beer(SQLModel, table=True):
-    id: Optional[int] = Field(primary_key=True)
+    id: Optional[int] = Field(primary_key=True, default=None, index=True)
     name: str
     style: str
     flavor: int
     image: int
     cost: int
+    date: datetime = Filed(default_factory=datetime.now)
+
+    @validator("image", "flavor", "cost")
+    def validate_rantings(cls, v, field):
+        if v < 1 or v > 10:
+            raise RuntimeError(f"{filed.name} must be between 1 and 10")
+        return v
+
+    @validator("rate", always=True)
+    def calculate_rate(cls, v, values):
+        rate = mean(
+            [
+                values["flavor"], 
+                values["image"],
+                values["cost"]
+            ]
+        )
+        return int(rate)
 
 brewdog = Beer(name="Brewdog", style="NEIPA", flavor=6, image=7, cost=8)
