9801ff568bee6ee3d7b6c1f6cf52692faa2de22e
Luís Gustavo <luisgsilva270@gmail.com>
2022-04-26 18:52:55 +0000

    import Session and created function get_session()

--- beerlog/database.py
+++ beerlog/database.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, Session
 
 from beerlog import models
 from beerlog.config import settings
@@ -6,3 +6,6 @@ from beerlog.config import settings
 
 engine = create_engine(settings.database.url, echo=False)
 models.SQLModel.metadata.create_all(engine)
+
+def get_session():
+    return Session(engine)
