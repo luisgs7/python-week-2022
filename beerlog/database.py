a184429987293eed5dbdb39d5cc90def4885598a
Luís Gustavo <luisgsilva270@gmail.com>
2022-04-26 18:23:42 +0000

    update database.py

--- beerlog/database.py
+++ beerlog/database.py
@@ -1,7 +1,16 @@
+import warnings
+from sqlalchemy.exc import SAWarning
+from sqlmodel.sql.expression import Select, SelectOfScalar
+
 from sqlmodel import create_engine
 
 from beerlog import models
 from beerlog.config import settings
+
+warnings.filterwarnings("ignore", category=SAWarning)
+SelectOfScalar.inherit_cache = True
+Select.inherit_cache = True
+
 
 engine = create_engine(settings.database.url, echo=False)
 models.SQLModel.metadata.create_all(engine)
